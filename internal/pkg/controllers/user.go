3c04b4a723b9f35db31956719f75eb42ece1058b
imayavgi <g52opal@yahoo.com>
2020-04-30 07:42:51 -0400

    updated allservices

--- internal/pkg/controllers/user.go
+++ internal/pkg/controllers/user.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"regexp"
+	"strconv"
+
+	"github.com/imayavgi/gows/internal/pkg/models"
 )
 
 type userController struct {
@@ -16,5 +20,112 @@ func newUserController() *userController {
 }
 
 func (uc userController) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte(" Simple response from User Controller"))
+	if request.URL.Path == "/users" {
+		switch request.Method {
+		case http.MethodGet:
+			uc.getAll(response, request)
+		case http.MethodPost:
+			uc.post(response, request)
+		default:
+			response.WriteHeader(http.StatusNotImplemented)
+		}
+	} else {
+		matches := uc.userIDPattern.FindStringSubmatch(request.URL.Path)
+		if len(matches) == 0 {
+			response.WriteHeader(http.StatusNotFound)
+		}
+		id, err := strconv.Atoi(matches[1])
+		if err != nil {
+			response.WriteHeader(http.StatusNotFound)
+		}
+		switch request.Method {
+		case http.MethodGet:
+			uc.get(id, response)
+		case http.MethodPut:
+			uc.put(id, response, request)
+		case http.MethodDelete:
+			uc.delete(id, response)
+		default:
+			response.WriteHeader(http.StatusNotImplemented)
+		}
+	}
+}
+
+func (uc *userController) getAll(response http.ResponseWriter, request *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(), response)
+}
+
+func (uc *userController) get(id int, response http.ResponseWriter) {
+	u, err := models.GetUserByID(id)
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	encodeResponseAsJSON(u, response)
+}
+
+func (uc *userController) post(response http.ResponseWriter, request *http.Request) {
+	u, err := uc.parseRequest(request)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte("Could not parse User object"))
+		return
+	}
+
+	u, err = models.AddUser(u)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(err.Error()))
+		return
+	}
+
+	encodeResponseAsJSON(u, response)
+}
+
+func (uc *userController) put(id int, response http.ResponseWriter, request *http.Request) {
+	u, err := uc.parseRequest(request)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte("Could not parse User object"))
+		return
+	}
+
+	if id != u.ID {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte("ID passed did match ID in URL"))
+		return
+	}
+
+	u, err = models.UpdateUser(u)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(err.Error()))
+		return
+	}
+
+	encodeResponseAsJSON(u, response)
+}
+
+func (uc *userController) delete(id int, response http.ResponseWriter) {
+	err := models.RemoveUserByID(id)
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(err.Error()))
+		return
+	}
+
+	response.WriteHeader(http.StatusOK)
+}
+
+func (uc *userController) parseRequest(request *http.Request) (models.User, error) {
+	dec := json.NewDecoder(request.Body)
+	var u models.User
+	err := dec.Decode(&u)
+	if err != nil {
+		return models.User{}, err
+	}
+	return u, nil
 }
--- internal/pkg/models/user.go
+++ internal/pkg/models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*
 User object
 */
@@ -21,8 +26,43 @@ func GetUsers() []*User {
 
 //AddUser ...
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("ID must not be preset")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+//GetUserByID ...
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+//UpdateUser ...
+func UpdateUser(tu User) (User, error) {
+	for i, u := range users {
+		if u.ID == tu.ID {
+			users[i] = &tu
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", tu.ID)
+}
+
+//RemoveUserByID ...
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
